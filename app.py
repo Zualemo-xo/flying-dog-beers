3b6b380d64687603673c5b2e75cff9257f1c48a8
Zualemo-xo <61049979+Zualemo-xo@users.noreply.github.com>
2020-06-30 04:25:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import dash
+'''import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
@@ -60,4 +60,75 @@ app.layout = html.Div(children=[
 )
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server()'''
+
+#vid 5
+#working code for live graph vidp4
+import dash
+from dash.dependencies import Output, Input
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly
+import random
+import plotly.graph_objs as go
+from collections import deque
+
+X = deque(maxlen=20)
+X.append(1)
+Y = deque(maxlen=20)
+Y.append(1)
+
+
+app = dash.Dash(__name__)
+app.layout = html.Div(
+    [
+        html.H1("Live Sentiment Graph",style={"background-color":"red"}),
+        dcc.Input(id='sentiment_term', value='lockdown', type='text'),
+        dcc.Graph(id='live-graph', animate=True),
+        dcc.Interval(
+            id='graph-update',
+            interval=1000,
+            n_intervals = 0
+        ),
+    ]
+)
+
+@app.callback(Output('live-graph', 'figure'),
+              [
+              Input(component_id='sentiment_term', component_property='value'),
+              Input('graph-update', 'n_intervals')]
+              )
+def update_graph_scatter(sentiment_term,n):
+    try:
+        conn = sqlite3.connect('twitter.db')
+        c = conn.cursor()
+        df = pd.read_sql("SELECT * FROM sentiment WHERE tweet LIKE ? ORDER BY unix DESC LIMIT 1000", conn ,params=('%' + sentiment_term + '%',))
+        df.sort_values('unix', inplace=True)
+        df['sentiment_smoothed'] = df['sentiment'].rolling(int(len(df)/2)).mean()
+
+        df['date'] = pd.to_datetime(df['unix'],unit='ms')
+        df.set_index('date', inplace=True)
+
+        df = df.resample('1min').mean()
+        df.dropna(inplace=True)
+        X = df.index
+        Y = df.sentiment_smoothed
+
+        data = plotly.graph_objs.Scatter(
+                x=X,
+                y=Y,
+                name='Scatter',
+                mode= 'lines+markers'
+                )
+
+        return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),
+                                                    yaxis=dict(range=[min(Y),max(Y)]),
+                                                    title='Term: {}'.format(sentiment_term))}
+
+    except Exception as e:
+        with open('errors.txt','a') as f:
+            f.write(str(e))
+            f.write('\n')
+
+if __name__ == '__main__':
+    app.run_server(debug=True,use_reloader=False)
