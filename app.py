754674fb51f44220196f6d5d22704b0c0c4edf7a
Zualemo-xo <61049979+Zualemo-xo@users.noreply.github.com>
2020-06-30 04:32:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ app.layout = html.Div(children=[
 if __name__ == '__main__':
     app.run_server()'''
 
-#vid 5
+'''#vid 5
 #working code for live graph vidp4
 import dash
 from dash.dependencies import Output, Input
@@ -129,6 +129,59 @@ def update_graph_scatter(sentiment_term,n):
         with open('errors.txt','a') as f:
             f.write(str(e))
             f.write('\n')
+
+if __name__ == '__main__':
+    app.run_server(debug=True,use_reloader=False)'''
+
+#vid4
+import dash
+from dash.dependencies import Output, Input
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly
+import random
+import plotly.graph_objs as go
+from collections import deque
+
+X = deque(maxlen=20)
+X.append(1)
+Y = deque(maxlen=20)
+Y.append(1)
+
+
+app = dash.Dash(__name__)
+app.layout = html.Div(
+    [
+        dcc.Graph(id='live-graph', animate=True),
+        dcc.Interval(
+            id='graph-update',
+            interval=1000,
+            n_intervals = 0
+        ),
+    ]
+)
+
+@app.callback(Output('live-graph', 'figure'),
+        [Input('graph-update', 'n_intervals')])
+
+
+def update_graph_scatter(n):
+    X.append(X[-1]+1)
+    Y.append(Y[-1]+Y[-1]*random.uniform(-0.1,0.1))
+
+    data = plotly.graph_objs.Scatter(
+            x=list(X),
+            y=list(Y),
+            name='Scatter',
+            mode= 'lines+markers'
+            )
+
+    return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),
+                                                yaxis=dict(range=[min(Y),max(Y)]),)}
+
+
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True,use_reloader=False)
